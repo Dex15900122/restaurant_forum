5d2335a2f332526398e87df6b48e95d75b3f6e01
admin <admin@admins-MacBook-Pro.local>
2017-12-16 12:16:03 +0800

    add restaurant show action and view for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -22,6 +22,16 @@ class Admin::RestaurantsController < ApplicationController
     end
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+
+
+
+
+  
+
   private
 
   def restaurant_params
