00df4fa09fe8fa351e3c748eee22c4311b10c95e
admin <admin@admins-MacBook-Pro.local>
2017-12-18 14:05:27 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,14 +1,15 @@
 class Admin::CategoriesController < ApplicationController
 
- before_action :authenticate_user!
+
  before_action :authenticate_admin
+ before_action :set_category, only: [:update, :destroy]
 
 
 def index
   @categories = Category.all
 
   if params[:id]
-    @category = Category.find(params[:id])
+    set_category
   else
     @category = Category.new
   end
@@ -26,7 +27,7 @@ def create
 end
 
 def update
-  @category = Category.find(params[:id])
+  
   if @category.update(category_params)
     redirect_to admin_categories_path
     flash[:notice] = "category was successfully updated"
@@ -38,7 +39,7 @@ end
 
 
   def destroy
-    @category = Category.find(params[:id])
+    
     @category.destroy
     flash[:alert] = "category was successfully deleted"
     redirect_to admin_categories_path
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,9 @@
 class RestaurantsController < ApplicationController
 
-   before_action :authenticate_user!
+  
   def index
   end
+  
 
 
 
