f12056a239de61e128eb7f7f78f335763713773c
admin <admin@admins-MacBook-Pro.local>
2017-12-19 20:12:06 +0800

    create comment model and setup relation between restaurant, user and comment

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,7 @@ class Restaurant < ApplicationRecord
 validates_presence_of :name
 mount_uploader :image, PhotoUploader
 belongs_to :category
+has_many :comments
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+has_many :comments
+
+
+
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -8,6 +15,20 @@ class User < ApplicationRecord
     self.role == "admin"
   
     end
+
+validates_presence_of :name
+ 
+    def first_name
+       self.name.split.first
+    end
+    def last_name
+     self.name.split.last_name
+    end
+
+
+
+
+
 
 
 end
